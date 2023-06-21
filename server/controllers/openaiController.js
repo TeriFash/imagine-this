6720834413bffb203d5f754b6f343104c30c4e96
Teri <teri.fash@gmail.com>
2023-06-21 10:43:39 +0300

    Update openaiController.js

--- server/controllers/openaiController.js
+++ server/controllers/openaiController.js
@@ -1,7 +1,8 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  // apiKey: process.env.OPENAI_API_KEY,
+  apiKey: 'ATATT3xFfGF0qq1ITJh39mtk0MWeXFLOPfrUmELjl6QSnxkdDHXGlSduzO8H8UokDJkM5OwStQaiPC-rj4eOdAKvPokKR2gNRYIicfE8UfuQBTvHmD75Ke3dhY2SaajyDRqc8aLJIAnqriDC1aLolCI420xlpRNU3LSzLfwLN7GuBsJWxoMWDrs=BE0D9359'
 });
 const openai = new OpenAIApi(configuration);
 
@@ -11,7 +12,7 @@ const generateImage = async (req, res) => {
     const response = await openai.createImage({
       prompt,
       n: 1,
-      size: '256x256',
+      size: '512x512',
     });
     const imageUrl = response.data.data[0].url;
     res.status(200).json({ success: true, data: imageUrl });
